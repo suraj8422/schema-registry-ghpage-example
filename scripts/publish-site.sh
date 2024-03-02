80b348ebedac82a96b7de5c4c47bd97f021b79e8
Suraj Bhan <suraj.bhan.-nd@disneystreaming.com>
2024-03-02 23:14:55 +0530

    updated script

--- scripts/publish-site.sh
+++ scripts/publish-site.sh
@@ -4,19 +4,22 @@ set -euo pipefail
 readonly repo='jenkins-pipeline-test'
 
 echo $PWD
-echo $SCHEMA_NAMES
+
 readonly repoDir=$(pwd)/gh-pages
 rm -rf $repoDir
 mkdir -p $repoDir
+echo $SCHEMA_NAMES
 
-git clone -b gh-pages "https://$GITHUB_TOKEN@github.com/suraj8422/schema-registry-ghpage-example/$repo" $repoDir
-cd $repoDir
+echo "${schemaNames}"
 
-docker run --rm --privileged \
-  -v $repoDir:/spec \
-  artifactory.prod.hulu.com/hulu-docker/telemetry/smithy-to-openapi/smithy-to-openapi:main \
-  -o /spec/index.html
-echo $repoDir
-git add index.html
-git diff-index --quiet HEAD || git commit -m 'updated gh-pages'
+#git clone -b gh-pages "https://$GITHUB_TOKEN@github.com/suraj8422/schema-registry-ghpage-example/$repo" $repoDir
+#cd $repoDir
+#
+#docker run --rm --privileged \
+#  -v $repoDir:/spec \
+#  artifactory.prod.hulu.com/hulu-docker/telemetry/smithy-to-openapi/smithy-to-openapi:main \
+#  -o /spec/index.html
+#echo $repoDir
+#git add index.html
+#git diff-index --quiet HEAD || git commit -m 'updated gh-pages'
 #git push origin gh-pages    # Need write access to push into github
