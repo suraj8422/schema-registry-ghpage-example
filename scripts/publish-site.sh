a3ee88cd3488f8af5a566fdf4b7e84849f83e768
Suraj Bhan <suraj.bhan.-nd@disneystreaming.com>
2024-03-04 11:38:11 +0530

    Update publish-site.sh

--- scripts/publish-site.sh
+++ scripts/publish-site.sh
@@ -9,10 +9,16 @@ echo $PWD
 array=("element1" "element2" "element3" "element4")
 
 # Iterate over the array and print each element separated by a space
+concatenated= ""
 for element in "${array[@]}"; do
     echo -n "$element "
-done
 
+    concatRes+="$element "
+
+    concatenated=$(printf "%s " "$element")
+    echo "$concatenated"
+done
+echo "$concatenated"
 
 #awk '$1 == "-"{ if (key == "Fruits:") print $NF; next } {key=$1}' schema.yml
 schema_data=$(sed -En '/:/h;G;s/^- (.*)\nSchemas:/\1/p' schema.yml)
