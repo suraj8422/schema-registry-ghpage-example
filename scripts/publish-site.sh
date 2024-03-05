99d51bc9e7bd100ee3feceb90cf9eb0fd98b966a
Suraj Bhan <suraj.bhan.-nd@disneystreaming.com>
2024-03-05 08:16:46 +0530

    Update publish-site.sh

--- scripts/publish-site.sh
+++ scripts/publish-site.sh
@@ -31,20 +31,38 @@ readonly repoDir="$PWD/gh-pages"
 rm -rf $repoDir
 mkdir -p $repoDir
 
+# Declare a schema registry name array
+schema_array=("schema-registry-tlmt-viewport" "schema-registry-tlmt-pq")
+
+# Iterate over the array and mapping each schema name separated by a space
+for schema in "${schema_array[@]}";
+do
+    schema_name+="$schema "
+done
+echo $schema_name
+
 git clone -b gh-pages "https://$GITHUB_TOKEN@github.prod.hulu.com/suraj-bhan/$repo" $repoDir
 cd $repoDir
 
-for schemaName in "${schemaNames[@]}"
-do
- echo "Generating html schema documentation for: $schemaName"
-done 
+set +u
+if [[ -z "$(git config --global user.name)" ]]; then
+  echo 'setting git email and name...'
+  git config --global user.email "BotClientSignals@hulu.com"
+  git config --global user.name "BotClientSignals"
+fi
+set -u
 
-   
 docker run --rm --privileged \
   -v $repoDir:/spec \
-  artifactory.prod.hulu.com/hulu-docker/telemetry/smithy-to-openapi/smithy-to-openapi:main \
-  -o /spec/index.html
+  artifactory.prod.hulu.com/hulu-docker/telemetry/smithy-to-openapi/smithy-to-openapi:main $schema_name
+
+ls -l $repoDir
 echo $repoDir
-git add index.html
+git pull origin gh-pages
+echo $repoDir
+git add .
+
+echo $repoDir
+git add .
 git diff-index --quiet HEAD || git commit -m 'updated gh-pages'
 #git push origin gh-pages
